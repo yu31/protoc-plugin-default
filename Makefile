16392c4f450e825991a65a8943948cfb0cff7a3a
wuyu <wuyu@yunify.com>
2024-01-14 00:29:19 +0800

    Optimized the scripts for test cases of invalid options

--- Makefile
+++ Makefile
@@ -86,14 +86,14 @@ test-only:  ## Run test cases without generated codes.
 #bench-only:
 #	@[[ ${VERBOSE} = "yes" ]] && set -x; go test -test.bench="." -test.run="Benchmark" -benchmem -count=1 ./xgo/tests/cases/...;
 
-.PHONY: test-error-go
-test-error-go:   ## Run test cases for invalid parameters in tags for golang.
-test-error-go: compile
-	@[[ ${VERBOSE} = "yes" ]] && bash -x scripts/test_error_go.sh "${CASE}" || bash scripts/test_error_go.sh "${CASE}"
+.PHONY: test-invalid-go
+test-invalid-go: ## Run test cases for invalid parameters in tags for golang.
+test-invalid-go: compile
+	@[[ ${VERBOSE} = "yes" ]] && bash -x scripts/test_invalid_go.sh || bash scripts/test_invalid_go.sh
 
-.PHONY: test-error
-test-error:  ## Run test cases for invalid parameters in tags.
-test-error: test-error-go
+.PHONY: test-invalid
+test-invalid: ## Run test cases for invalid parameters in options.
+test-invalid: test-invalid-go
 
 # publishing java jar to central repository
 .PHONY: java-release
--- scripts/test_invalid_go.sh
+++ scripts/test_invalid_go.sh
@@ -9,39 +9,46 @@ export PATH="./build/bin:$PATH"
 PLUGIN="godefault"
 BASE_DIR="tests/proto/invalid"
 
-KIND="$1"
+# DEBUG/TYPE/FILE is set by environment variables;
+# e.g: DEBUG=yes TYPE=any FILE=any01.proto make test-invalid
+DEBUG="${DEBUG}"
+TYPE="${TYPE}"
+FILE="${FILE}"
 
-if [ -z "${KIND}" ]; then
-  KIND="*"
+if [ -z "${TYPE}" ]; then
+  TYPE="*"
+fi
+if [ -z "${FILE}" ]; then
+  FILE="*"
 fi
 
 function runCase() {
+  file="$1"
   echo "=== RUN:  ${file}"
 
   ret=$(protoc -I=. --go_out=./ --"${PLUGIN}"_out=./ "${file}" 2>&1)
 
-  if [ "${DEBUG}" != "no" ]; then
+  if [ "${DEBUG}" == "yes" ]; then
     echo "${ret}"
   fi
 
-  if echo "${ret}" |grep '\[ERROR\] - \[plugin: godefault\]' > /dev/null; then
+    if echo "${ret}" |grep "\[ERROR\] - \[plugin: ${PLUGIN}\]" > /dev/null; then
     echo "--- PASS: ${file}"
   else
-    echo "    Expected error by godefault, but it passing or other error occurs"
+    echo "    Expected error by ${PLUGIN}, but it passing or other error occurs"
     echo "--- FAIL: ${file}"
     exit 1
   fi
   return
 }
 
-for file in ${BASE_DIR}/${KIND}/*; do
+for file in ${BASE_DIR}/${TYPE}/${FILE}; do
   runCase "${file}"
 done
 
 echo "--------------------------------------------"
-echo "------ ALL test cases are successful ------"
+echo "------ ALL test cases are successful in language golang ------"
 echo "--------------------------------------------"
 
 # Delete generated files.
 /bin/rm -fr ./xgo/tests/pb/pbinvalid/*;
-
